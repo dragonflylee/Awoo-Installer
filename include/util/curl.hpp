0bd40380a8103d791ef06125062ee4043541f5f9
dragonflylee <dragonflylee@outlook.com>
2023-04-26 07:45:16 +0000

    add curl http client

--- include/util/curl.hpp
+++ include/util/curl.hpp
@@ -1,8 +1,27 @@
 #pragma once
 
 #include <string>
+#include <vector>
+#include <map>
 
 namespace inst::curl {
     bool downloadFile(const std::string ourUrl, const char *pagefilename, long timeout = 5000, bool writeProgress = false);
     std::string getRange(const std::string ourUrl, int firstRange = -1, int secondRange = -1, long timeout = 5000);
+
+    struct curl_ctx;
+
+    class client {
+    public:
+        client();
+        virtual ~client();
+        virtual const char*  userAgent() { return "Mozilla/5.0"; }
+
+        std::string get(const std::string& url, const std::vector<std::string>& headers);
+        std::string post(const std::string& url, const std::string& data, const std::vector<std::string>& headers);
+        std::string formEncode(const std::map<std::string, std::string>& form);
+
+    private:
+        curl_ctx* ctx;
+    };
+
 }
