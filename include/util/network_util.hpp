da1d5bae9a102ee89fc3bd39046002f47091665b
dragonflylee <dragonflylee@outlook.com>
2023-04-26 05:03:45 +0000

    tin::network: remove httpheader

--- include/util/network_util.hpp
+++ include/util/network_util.hpp
@@ -45,28 +45,10 @@ extern "C" {
 
 namespace tin::network
 {
-    class HTTPHeader
-    {
-        private:
-            std::string m_url;
-            std::map<std::string, std::string> m_values;
-
-            static size_t ParseHTMLHeader(char* bytes, size_t size, size_t numItems, void* userData);
-
-        public:
-            HTTPHeader(std::string url);
-
-            void PerformRequest();
-
-            bool HasValue(std::string key);
-            std::string GetValue(std::string key);
-    };
-
     class HTTPDownload
     {
         private:
             std::string m_url;
-            HTTPHeader m_header;
             bool m_rangesSupported = false;
 
             static size_t ParseHTMLData(char* bytes, size_t size, size_t numItems, void* userData);
