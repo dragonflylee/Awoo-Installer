c95436af7bccb45cc209741a5d9a77d21708ac75
dragonflylee <dragonflylee@outlook.com>
2023-04-27 14:05:00 +0000

    fix new drive

--- source/drive/drive.cpp
+++ source/drive/drive.cpp
@@ -4,27 +4,15 @@
 
 namespace inst::drive {
 
-static std::map<drive_type, drive::ref> driveMap;
-
 drive::ref new_drive(drive_type type) {
-    drive::ref client;
-    auto it = driveMap.find(type);
-    if (it == driveMap.end()) {
-        switch (type) {
-        case dt_httpdir:
-            client = std::make_shared<httpdir>();
-            break;
-        case dt_alidrive:
-            client = std::make_unique<aliyundrive>();
-            break;
-        default:
-            throw std::runtime_error("unsupport drive type");
-        }
-        driveMap.insert(std::make_pair(type, client));
-    } else {
-        client = it->second;
+    switch (type) {
+    case dt_httpdir:
+        return std::make_shared<httpdir>();
+    case dt_alidrive:
+        return std::make_shared<aliyundrive>();
+    default:
+        throw std::runtime_error("unsupport drive type");
     }
-    return client;
 }
 
 }
